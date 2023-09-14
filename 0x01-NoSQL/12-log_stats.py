00b40bb0706aac80e7ba3c8c83be2c19a69d4e5f
BM-Daniel <danbabereyir@gmail.com>
2023-09-14 17:12:27 +0000

    Task 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+'''
+Write a Python script that provides some stats about Nginx logs stored in
+MongoDB
+'''
+
+import pymongo
+
+
+if __name__ == "__main__":
+    client = pymongo.MongoClient()
+    nginx_logs = client.logs.nginx
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    logs_number = nginx_logs.count_documents({})
+    print(f"{logs_number} logs")
+
+    print("Methods:")
+    for i in methods:
+        method_number = nginx_logs.count_documents({'method': i})
+        print(f"\tmethod {i}: {method_number}")
+
+    verify = nginx_logs.count_documents({'method': 'GET', 'path': '/status'})
+    print(f"{verify} status check")
