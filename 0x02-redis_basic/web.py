542709c03d68790c158b400349762a537133a68d
BM-Daniel <danbabereyir@gmail.com>
2023-11-05 14:37:36 +0000

    advanced task

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-
 '''
 web cache and tracker
 '''
-
 import requests
 import redis
 from functools import wraps
@@ -12,14 +10,12 @@ store = redis.Redis()
 
 
 def count_url_access(method):
-    '''
-    Decorator counting how many times a URL is accessed
-    '''
+    ''' Decorator counting how many times
+    a URL is accessed '''
     @wraps(method)
     def wrapper(url):
         cached_key = "cached:" + url
         cached_data = store.get(cached_key)
-
         if cached_data:
             return cached_data.decode("utf-8")
 
@@ -29,9 +25,7 @@ def count_url_access(method):
         store.incr(count_key)
         store.set(cached_key, html)
         store.expire(cached_key, 10)
-
         return html
-
     return wrapper
 
 
@@ -39,5 +33,4 @@ def count_url_access(method):
 def get_page(url: str) -> str:
     ''' Returns HTML content of a url '''
     res = requests.get(url)
-
     return res.text
